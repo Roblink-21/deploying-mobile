ffd37a54997018be2f390bd31bf1a0152f954f20
ROBpin-21 <roberthlink@yahoo.es>
2022-06-08 19:34:33 -0500

    Mi gps

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'migps-6cd27',
+    appId: '1:139538316595:web:f007123a305ccae4f29246',
+    databaseURL: 'https://migps-6cd27-default-rtdb.firebaseio.com',
+    storageBucket: 'migps-6cd27.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyB9uFKVDZsh3Xx5ZkAqCP_5gQE9JejWKmY',
+    authDomain: 'migps-6cd27.firebaseapp.com',
+    messagingSenderId: '139538316595',
+  },
   production: false
 };
 
